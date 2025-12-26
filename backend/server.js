1aedceadf063ca58d6915ffb697fc1b87b2561fb
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-12-26 13:45:57 +0530

    add delete routes

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,13 @@ import express from "express";
 import cors from "cors";
 import notesRoutes from "./routes/notes.js";
 
-
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.use("/notes", notesRoutes);
 
-app.listen(5000, () =>
-  console.log("Backend running on http://localhost:5000")
-);
+app.listen(5000, () => {
+  console.log("Backend running on port 5000");
+});
